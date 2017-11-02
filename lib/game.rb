ab7cc5f733d0701eef7f858b86b1cc00af1d5a08
benjaminsunderland <ben@sunderland.eu.com>
2017-11-02 15:42:08 +0000

    Wrote test to check for horizontal winner

--- lib/game.rb
+++ lib/game.rb
@@ -6,9 +6,22 @@ class Game
 
   def initialize(board = Board.new)
     @board = board
+    @state = true
   end
 
   def player_make_move(row, col , player)
     @board.grid[row][col] = player
   end
+
+  def horizontal_winner
+    @board.grid.map do |x|
+    case x
+    when %w(o o o)
+      return "Winner"
+      end
+    end
+  end
+
+
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -18,4 +18,8 @@ describe Game do
       expect(game.board.grid[0][1]).to eq("x")
     end
   end
+
+  describe "when a player wants to win" do
+    it "is possible for a player to fill a row horizontally" do
+      expect(game.horizontal_winner).to eq(["x", "x", "x"])
 end
