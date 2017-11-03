de93f3777058597ffd651f627bfb2e14e4e0589c
benjaminsunderland <ben@sunderland.eu.com>
2017-11-03 12:02:03 +0000

    Deleted data for the winner methods and added position_taken? method

--- lib/game.rb
+++ lib/game.rb
@@ -2,51 +2,49 @@ require_relative 'board'
 
 class Game
 
-  attr_accessor :board
 
-  WIN_CASES = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],
-               [2,4,6],[0,3,6],[1,4,7],[2,5,8]]
+  attr_accessor :board
 
   def initialize(board = Board.new)
     @board = board
     @state = true
+    @turn = 0
   end
 
   def player_make_move(row, col , player)
     @board.grid[row][col] = player
   end
 
-  def horizontal_winner
-    if @board.grid.include?(nil)
-      return "Error"
-    elsif
-      (@board.grid[0][0] == @board.grid[0][2]) && (@board.grid[0][0] == @board.grid[0][1]) ||
-      (@board.grid[1][0] == @board.grid[1][2]) && (@board.grid[1][0] == @board.grid[1][1]) ||
-      (@board.grid[2][0] == @board.grid[2][2]) && (@board.grid[2][0] == @board.grid[2][1])
-      return "Winner"
+  def next_turn
+    @turn += 1
+  end
+
+  def current_turn
+    @turn.even? ? (return "X") : (return "O")
+  end
+
+  def position_taken?(row, col)
+    if (@board.grid[row][col] == nil)
+      return false
     else
-       return nil
+      return true
     end
+  end
+
+  def horizontal_winner
   end
 
   def vertical_winner
-    if
-      (@board.grid[0][0] == @board.grid[0][3]) && (@board.grid[0][0] == @board.grid[0][6]) ||
-      (@board.grid[1][0] == @board.grid[1][4]) && (@board.grid[1][0] == @board.grid[1][7]) ||
-      (@board.grid[2][0] == @board.grid[2][5]) && (@board.grid[2][0] == @board.grid[2][8])
-      return "Winner"
-    else
-      return "No Winner"
-    end
+
   end
 
   def diagonal_winner
-    if
-      (@board.grid[0][0] == @board.grid[0][4]) && (@board.grid[0][0] == @board.grid[0][8]) ||
-      (@board.grid[2][0] == @board.grid[2][4]) && (@board.grid[2][0] == @board.grid[2][6])
-      return "Winner"
-    else
-      return "No Winner"
-    end
+
+  end
+
+  def win?
+    return true if horizontal_winner
+    return true if vertical_winner
+    return true if diagonal_winner
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,17 +5,26 @@ describe Game do
   subject(:game) {described_class.new(board)}
   let(:board) {double :board, grid: [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]}
 
-
-
-
   describe "When a game is started" do
     it "it is initialized with a board" do
       expect(game.board).to eq(board)
+    end
+  end
+
+  describe "When a game is in action" do
+    it "is able to change player" do
+      game.next_turn
+      expect(game.current_turn).to eq("O")
     end
 
     it "is able for a player to make a move" do
       game.player_make_move(0, 1, "x")
       expect(game.board.grid[0][1]).to eq("x")
+    end
+
+    it "is able to see if a spot has been taken" do
+      game.player_make_move(0, 0, "x")
+      expect(game.position_taken?(0, 0)).to eq(true)
     end
   end
 
@@ -24,23 +33,21 @@ describe Game do
       game.player_make_move(0, 0, "x")
       game.player_make_move(0, 1, "x")
       game.player_make_move(0, 2, "x")
-      expect(game.horizontal_winner).to eq("Winner")
+      pending
     end
 
     it "is possible for a player to fill a row vertically" do
       game.player_make_move(0, 0, "x")
       game.player_make_move(0, 3, "x")
       game.player_make_move(0, 6, "x")
-      expect(game.vertical_winner).to eq("Winner")
+      pending
     end
 
     it "is possible for a player to fill a row diagonally" do
       game.player_make_move(0, 0, "x")
       game.player_make_move(0, 4, "x")
       game.player_make_move(0, 8, "x")
-      expect(game.diagonal_winner).to eq("Winner")
+      pending
     end
   end
-
-
 end
