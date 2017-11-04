d959b7112afe691ac71c9813fbf4ece71a28a956
benjaminsunderland <ben@sunderland.eu.com>
2017-11-04 15:24:26 +0000

    Added new tests for ggame class which stub out the behaviour of board

--- lib/game.rb
+++ lib/game.rb
@@ -13,13 +13,21 @@ class Game
     @board.position_taken?(row, col)
   end
 
-  def place_marker(row, col)
-    @board.current_turn
-    @board.player_make_move(row, col, player)
+  def out_of_bounds(row, col)
+    @board.off_board?(row, col)
+  end
+
+  def place_marker(row, col, player)
+    # @board.current_turn
+    if out_of_bounds(row, col) || taken?(row, col)
+      fail "You can't do that"
+    else
+      @board.player_make_move(row, col, player)
+    end
     @board.next_turn
   end
 
-  def win?
+  def winner
     @board.three_in_a_row?
     @board.game_over
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,7 @@ require 'game'
 describe Game do
 
   subject(:game) {described_class.new(board)}
-  let(:board) {double :board, grid: [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]], position_taken: true}
+  let(:board) {double :board, grid: [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]}
 
   before do
   allow(board).to receive(:position_taken)
@@ -17,8 +17,22 @@ end
 
   describe "During a game" do
     it "is possible for a position to be taken" do
-      allow(board).to receive(:position_taken).and_return(true)
-      expect(game.taken?(0,0)).to eq(true)
+      allow(board).to receive(:position_taken?).and_return(true)
+      expect(game.taken?(0,1)).to eq(true)
+    end
+
+    it "isn't possible to place a marker out of the grid" do
+      allow(board).to receive(:off_board?).and_return(true)
+      expect(game.out_of_bounds(0, -1)).to eq(true)
+    end
+
+    it "is possible to place your marker" do
+      allow(board).to receive(:current_turn).and_return("X")
+      allow(board).to receive(:position_taken?).and_return(false)
+      allow(board).to receive(:off_board?).and_return(false)
+      allow(board).to receive(:player_make_move).with(1, 1, "X")
+      allow(board).to receive(:next_turn).and_return(1)
+      game.place_marker(1, 1, "X")
     end
   end
 end
