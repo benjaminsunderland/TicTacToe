cd87f3e88d18a206cc9447f3de70dc55fb2cd054
benjaminsunderland <ben@sunderland.eu.com>
2017-11-04 11:06:58 +0000

    Added tests and code for wins (horizontally, vertically and diagonally).

--- lib/game.rb
+++ lib/game.rb
@@ -2,17 +2,25 @@ require_relative 'board'
 
 class Game
 
-
   attr_accessor :board
+  attr_reader :turn
+
+WINNING_COMBINATIONS = [[0,1,2],[3,4,5],
+                        [0,3,6],[6,7,8],
+                        [1,4,7],[2,5,8],
+                        [0,4,8],[2,4,6]]
 
   def initialize(board = Board.new)
     @board = board
-    @state = true
+    @in_game = true
     @turn = 0
   end
 
   def player_make_move(row, col , player)
+    # fail "The game is over!" if win?
+    fail "You can't do that!" if off_board?(row, col)
     @board.grid[row][col] = player
+    next_turn
   end
 
   def next_turn
@@ -23,6 +31,10 @@ class Game
     @turn.even? ? (return "X") : (return "O")
   end
 
+  def off_board?(row, col)
+   return true if (row < 0 || row > 2) || (col < 0 || col > 2)
+  end
+
   def position_taken?(row, col)
     if (@board.grid[row][col] == nil)
       return false
@@ -31,28 +43,25 @@ class Game
     end
   end
 
-  def horizontal_winner?
-    @board.grid.each do |x| x.detect do |e|
-       if x.count(e) >= 3
-         return true
-        else
-         return false
-        end
+  def three_in_a_row?
+    flat_board = @board.grid.flatten
+    x_matches = flat_board.each_index.select {|i| flat_board[i] == "X"}
+    o_matches = flat_board.each_index.select {|i| flat_board[i] == "O"}
+
+    WINNING_COMBINATIONS.each do |line|
+      if (line - x_matches).empty?
+        return true
+      elsif (line - o_matches).empty?
+  		  return true
       end
-    end
-  end
-
-  def vertical_winner
-
-  end
-
-  def diagonal_winner
-
+  	end
+  	return false
   end
 
   def win?
-    return true if horizontal_winner
-    return true if vertical_winner
-    return true if diagonal_winner
+    return true if horizontal_winner?
+    return true if vertical_winner?
+    return true if diagonal_winner?
+    @in_game = false
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -22,32 +22,48 @@ describe Game do
       expect(game.board.grid[0][1]).to eq("x")
     end
 
+    it 'is possible for players to take turns during the game' do
+      game.next_turn
+      expect(game.turn).to eq(1)
+    end
+
     it "is able to see if a spot has been taken" do
       game.player_make_move(0, 0, "x")
       expect(game.position_taken?(0, 0)).to eq(true)
+    end
+
+    it "isn't possible to place a marker off the board" do
+      expect(game.off_board?(0, -1)).to eq(true)
+    end
+
+    it "will create an error if you place a player off the board" do
+      expect{game.player_make_move(0, -1, "x")}.to raise_error("You can't do that!")
     end
   end
 
   describe "when a player wants to win" do
     it "is possible for a player to fill a row horizontally" do
-      game.player_make_move(0, 0, "x")
-      game.player_make_move(0, 1, "x")
-      game.player_make_move(0, 2, "x")
-      expect(game.horizontal_winner?).to eq(true)
+      game.player_make_move(0, 0, "X")
+      game.player_make_move(0, 1, "X")
+      game.player_make_move(0, 2, "X")
+      expect(game.three_in_a_row?).to eq(true)
     end
 
     it "is possible for a player to fill a row vertically" do
-      game.player_make_move(0, 0, "x")
-      game.player_make_move(0, 3, "x")
-      game.player_make_move(0, 6, "x")
-      pending
+      game.player_make_move(0, 0, "O")
+      game.player_make_move(1, 0, "O")
+      game.player_make_move(2, 0, "O")
+      expect(game.three_in_a_row?).to eq(true)
     end
 
-    it "is possible for a player to fill a row diagonally" do
-      game.player_make_move(0, 0, "x")
-      game.player_make_move(0, 4, "x")
-      game.player_make_move(0, 8, "x")
-      pending
+
+      it "is possible for a player to fill a row diagonally" do
+      game.player_make_move(0, 0, "O")
+      game.player_make_move(1, 1, "O")
+      game.player_make_move(2, 2, "O")
+      expect(game.three_in_a_row?).to eq(true)
     end
+
+
   end
 end
